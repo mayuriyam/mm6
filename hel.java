133afef34c4f0640ba3bc5ece99372bdcf09480c
mayuriyam <mayuriya21@gmail.com>
2017-06-15 10:21:54 +0530

    Create hel.java

---
+++ hel.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class hel
+{
+    public static void main(String ar[])
+    {
+        Scanner sc=new Scanner (System.in);
+        String s="hello";
+        
+          System.out.println(s+" "+s+" "+s+" "+s+" "+s);
+        
+    }
+}
